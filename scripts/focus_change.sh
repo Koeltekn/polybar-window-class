0481f9fb5a1537ab5b58fd689e8a5709234011cd
Koeltekn <83598852+Koeltekn@users.noreply.github.com>
2021-08-09 00:28:00 +0200

    Added support for multiple bars

--- scripts/focus_change.sh
+++ scripts/focus_change.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+BAR="BAR"
+BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
+
 xprop -spy -root _NET_ACTIVE_WINDOW | while read change;
 do
-   polybar-msg -p $(pidof polybar) hook class 1
+   polybar-msg -p $BAR_PID hook class 1
 done
