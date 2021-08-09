80f04a2d393655d2a47e890cbfbdfaae7a2e7d04
Koeltekn <83598852+Koeltekn@users.noreply.github.com>
2021-08-09 16:07:55 +0200

    added function thingies

--- scripts/focus_change.sh
+++ scripts/focus_change.sh
@@ -2,19 +2,23 @@
 
 # CHANGE TO NAME OF THE USED BAR
 BAR="bar"
-BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
 
-while [[ $BAR_PID = "" ]]; do
-   BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
-   sleep 0.1
-done
+get_bar_pid () {
+   echo "$(pgrep -a "polybar" | grep "$1" | cut -d" " -f1)"
+}
+
+update_bar_pid () {
+   local PID="$(get_bar_pid "$1")"
+   while [[ $PID = "" ]]; do
+      PID="$(get_bar_pid "$1")"
+      sleep 0.1
+   done
+   echo "$PID"
+}
+
 
 xprop -spy -root _NET_ACTIVE_WINDOW | while read change;
 do
-   BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
-   while [[ $BAR_PID = "" ]]; do
-      BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
-      sleep 0.1
-   done
+   BAR_PID="$(update_bar_pid "$BAR")"
    polybar-msg -p $BAR_PID hook class 1
 done
