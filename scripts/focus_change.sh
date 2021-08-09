389bfc7ffecd642f53413399a280ddec19fbb906
Koeltekn <83598852+Koeltekn@users.noreply.github.com>
2021-08-09 12:23:41 +0200

    Fixed loading thingy

--- scripts/focus_change.sh
+++ scripts/focus_change.sh
@@ -4,6 +4,11 @@
 BAR="BAR"
 BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
 
+while [[ $BAR_PID = "" ]]; do
+   BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
+   sleep 0.2
+done
+
 xprop -spy -root _NET_ACTIVE_WINDOW | while read change;
 do
    polybar-msg -p $BAR_PID hook class 1
