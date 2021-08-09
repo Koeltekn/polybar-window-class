84e5e6c8928c82398cf8404ff28bcdfc8ca6717a
Koeltekn <83598852+Koeltekn@users.noreply.github.com>
2021-08-09 15:43:23 +0200

    Fixed pid if bar restarts

--- scripts/focus_change.sh
+++ scripts/focus_change.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
 # CHANGE TO NAME OF THE USED BAR
-BAR="BAR"
+BAR="bar"
 BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
 
 while [[ $BAR_PID = "" ]]; do
    BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
-   sleep 0.2
+   sleep 0.1
 done
 
 xprop -spy -root _NET_ACTIVE_WINDOW | while read change;
 do
+   BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
+   while [[ $BAR_PID = "" ]]; do
+      BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
+      sleep 0.1
+   done
    polybar-msg -p $BAR_PID hook class 1
 done
